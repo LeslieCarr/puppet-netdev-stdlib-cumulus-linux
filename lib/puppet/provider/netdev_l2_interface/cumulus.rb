99d414268b664238d6b001a24e283a5cc4ae3db2
Sergey Sudakovich <sergey@cumulusnetworks.com>
2013-07-09 15:27:35 -0700

    l2_interface discovery

--- lib/puppet/provider/netdev_l2_interface/cumulus.rb
+++ lib/puppet/provider/netdev_l2_interface/cumulus.rb
@@ -1,5 +1,7 @@
 Puppet::Type.type(:netdev_l2_interface).provide(:cumulus) do
 
+  commands  :iplink => '/sbin/ip'
+
   mk_resource_methods
 
   def exists?
@@ -7,12 +9,20 @@ Puppet::Type.type(:netdev_l2_interface).provide(:cumulus) do
   end
 
   class << self
+    def link_master name
+      iplink(['-oneline','link', 'show', name ]).match(/master\s+(\S+)/)
+    end
+
     def instances
       interfaces = Puppet::Type.type(:netdev_interface).instances.map {|i| i[:name]}
       bridges = Puppet::Type.type(:netdev_vlan).instances.map {|i| i[:name]}
       l2_interfaces = interfaces - bridges
       l2_interfaces.collect do |i|
         new ({:name => i,
+              :untagged_vlan => link_master(i) || :absent,
+              :tagged_vlans => Dir[File.join SYSFS_NET_PATH, i + '.*'].collect do |subi|
+                link_master(File.basename subi)
+              end,
               :ensure => :present})
 
       end
