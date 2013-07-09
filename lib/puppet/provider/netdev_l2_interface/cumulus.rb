ececc8c3a052ce1c7a22a02ef2dd9e8bf09ae1d2
Sergey Sudakovich <sergey@cumulusnetworks.com>
2013-07-08 17:06:19 -0700

    added netdev_vlan

--- lib/puppet/provider/netdev_l2_interface/cumulus.rb
+++ lib/puppet/provider/netdev_l2_interface/cumulus.rb
@@ -1,3 +1,2 @@
 Puppet::Type.type(:netdev_l2_interface).provide(:cumulus) do
-
 end
--- lib/puppet/provider/netdev_vlan/cumulus.rb
+++ lib/puppet/provider/netdev_vlan/cumulus.rb
@@ -1,2 +1,31 @@
 Puppet::Type.type(:netdev_vlan).provide(:cumulus) do
+
+  commands :brctl =>'/sbin/brctl'
+
+  mk_resource_methods
+
+  def create
+    brctl(['addbr', resource[:name]])
+  end
+
+  def destroy
+    brctl(['delbr', resource[:name]])
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present
+  end
+
+  class << self
+
+    def instances
+      bridges = brctl('show').lines[1..-1]
+      bridges.each.collect do |br|
+        name, id, stp, interfaces = br.split
+        new ({:name => name,
+              :ensure => :present})
+      end
+    end
+  end
+
 end
