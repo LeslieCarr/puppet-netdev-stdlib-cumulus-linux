af6ea811a4be8f00b4ce31c0f055ddda9cbbc189
Sergey Sudakovich <sergey@cumulusnetworks.com>
2013-08-15 12:00:02 -0700

    Can persist VLAN configuration

--- lib/puppet/provider/netdev_vlan/cumulus.rb
+++ lib/puppet/provider/netdev_vlan/cumulus.rb
@@ -1,3 +1,5 @@
+require File.join(File.dirname(__FILE__), '..', 'cumulus', 'network_interfaces.rb')
+
 Puppet::Type.type(:netdev_vlan).provide(:cumulus) do
 
   commands :brctl =>'/sbin/brctl',
@@ -9,12 +11,25 @@ Puppet::Type.type(:netdev_vlan).provide(:cumulus) do
 
   mk_resource_methods
 
+  def initialize(value={})
+    super(value)
+    @property_flush = {}
+    NetworkInterfaces.parse
+  end
+
   def create
     unless resource[:name] =~ /^\w+#{NAME_SEP}\d+/
       raise ArgumentError, "VLAN name must be in format <name>#{NAME_SEP}<VLAN ID>"
     end
     brctl(['addbr', resource[:name]])
     iplink(['link', 'set', 'dev', resource[:name], 'up'])
+    NetworkInterfaces[resource[:name]].family = 'inet'
+    NetworkInterfaces[resource[:name]].method = 'manual'
+    NetworkInterfaces[resource[:name]].onboot = true
+    NetworkInterfaces[resource[:name]].options['bridge_stp'] << 'on'
+    NetworkInterfaces[resource[:name]].options['bridge_maxwait'] << 20
+    NetworkInterfaces[resource[:name]].options['bridge_ageing'] << 200
+    NetworkInterfaces[resource[:name]].options['bridge_fd'] << 30
   end
 
   def destroy
@@ -29,11 +44,26 @@ Puppet::Type.type(:netdev_vlan).provide(:cumulus) do
     raise "VLAN can not be renamed."
   end
 
+  def vlan_id=(value)
+    raise "VLAN ID can not be changed."
+  end
+
   def no_mac_learning=(value)
     #To disable mac learning set ageing time to zero
     #Otherwise set to default (300 seconds)
-    brctl(['setageing', resource[:name], value ? 0 : DEFAULT_AGING_TIME])
+    # brctl(['setageing', resource[:name], value ? 0 : DEFAULT_AGING_TIME])
+    @property_flush[:no_mac_learning] = value
+
   end
+
+  def self.flush
+    if @property_flush[:no_mac_learning]
+      ageing = @property_flush[:no_mac_learning] ? 0 : DEFAULT_AGING_TIME
+      brctl(['setageing', resource[:name], ageing])
+      NetworkInterfaces[resource[:name]].options['bridge_ageing'] = [ageing]
+    end
+  end
+
 
   def self.instances
     bridges = Dir[File.join SYSFS_NET_PATH, '*'].
